4980470a602d9f8826e29c22da9c55c4c5b5e717
samwanyua <samexample8@gmail.com>
2023-10-12 22:12:30 +0300

    Multiple assignments,string methods and typecasting

---
+++ multiple assignment.py
@@ -0,0 +1,9 @@
+name, age, rich = "Sam", 23, True
+print(name)
+print(age)
+print(rich)
+
+x=y=z=289
+print(x)
+print(y)
+print(z)
---
+++ string methods.py
@@ -0,0 +1,11 @@
+name= "josephatomondi"
+print(len(name)) #length of the word Josephat
+print(name.find("o")) #finding where character "o" is, we start with 0,1,2,3....
+print(name.capitalize()) #this will capitalize the character 'J' if it was written in lowercase
+print(name.upper()) #Uppecase
+print(name.lower()) #lowercase
+print(name.isdigit()) #this are letters per se hence the answer 'False'
+print(name.isalpha()) # True because they are alphabets
+print(name.count("o")) #Counts the no. of 'o'
+print(name.replace("o", "a")) #replaces every letter 'o' with 'a'
+print(name*46)
