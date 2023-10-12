66700fcfb3082a163197fd9d789c5389c3836e0f
samwanyua <samexample8@gmail.com>
2023-10-12 22:27:07 +0300

    Slicing

---
+++ slicing.py
@@ -0,0 +1,9 @@
+name= "Wanyua"
+first_name = name[0:5]
+funky_name = name[0:11:3]
+reversed_name = name[::-2]
+print(reversed_name)
+
+website="http://google.com"
+slice = slice(7,-4)
+print(website[slice])
