7a59b16cb6a0644a2cc7a503168c9b7cfc8ea4f6
samwanyua <samexample8@gmail.com>
2023-10-12 22:03:26 +0300

    Variables

---
+++ varibles.py
@@ -0,0 +1,19 @@
+first_name = "Wesley"
+last_name = "John"
+full_name = first_name + " " +last_name
+
+print(type(first_name))
+print("Niaje " + full_name)
+
+age = 23
+age = age + 45  #age += 45 is a shortcut
+print(age)
+print("You are age " + str(age) + " Years old")
+
+height  = 458.4345
+print("Your height is " + str(height) + " millimeter tall.")
+print(type(height))
+
+human = True
+print(type(human))
+print("Are you a human? " + str(human))
