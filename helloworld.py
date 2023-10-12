c235c04d7d59044ce4604525844e529e8fcf2c22
samwanyua <samexample8@gmail.com>
2023-10-12 20:52:28 +0300

    Hello World

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print("Hello world")
+print("I love python")
+
+first_name = "Wesley"
+last_name = "John"
+full_name = first_name + " " +last_name
+
+print(type(first_name))
+print("Hello " + full_name)
