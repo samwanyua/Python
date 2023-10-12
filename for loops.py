37c571b8021402d81974db6df90fbace20c60be3
samwanyua <samexample8@gmail.com>
2023-10-12 22:39:53 +0300

    For loops

---
+++ for loops.py
@@ -0,0 +1,13 @@
+for i in range (23):
+      print(i+200)
+
+for i in range (23, 60+1,3):
+      print(i)
+
+for i in ("Nebchadnezzar"):
+      print(i)
+
+for seconds in range (10, 0, -1):
+      print(seconds)
+      #time.sleep(1)
+print("Happy New Year!!!")
