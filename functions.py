52cca2d392e9955c3624fa4a6324f139ab9d785d
samwanyua <samexample8@gmail.com>
2023-10-19 14:51:58 +0300

    Functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+#Functions
+
+def Jambo(name):
+    print("Hello Sam " + name)
+    print("I need thee every hour")
+    print("Oh God my GOd")
+Jambo("Juba")
+Jambo("Juba")
+Jambo("Juba")
+
+def hello(first_name, last_name):
+    print("Hello " + first_name + " "  + last_name)
+    print("Have a nice day")
+my_name="Bro"
+hello("Bro", "Code")
+
+def hello(first_name, last_name, age):
+     print("Hello " + first_name + " "  + last_name)
+     print("You are " + str(age)+ " years old.")
+my_name="Bro"
+hello("Bro", "Code", 21 )
