fd63dc5e37f113bbf7d43e4e482e403a2561d31a
samwanyua <samexample8@gmail.com>
2023-10-20 15:10:32 +0300

    Return statements

---
+++ return-statements.py
@@ -0,0 +1,12 @@
+#Return Statements
+
+def multiply (number1, number2):
+    result=number1 * number2
+    return result
+
+x= multiply(6,8)
+print(x)
+
+def hello(first, middle, last):
+    print("Hello " + first + " " + middle + " "  + last)
+hello(first="Sam", last="Wanyua" , middle="Wanyoro")
