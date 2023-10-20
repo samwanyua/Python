2f103b2b6b547c5ed7da26f6085b3782f0c0052d
samwanyua <samexample8@gmail.com>
2023-10-20 15:13:11 +0300

    Nested function calls

---
+++ nested-function-calls.py
@@ -0,0 +1,9 @@
+#Nested function calls
+
+num= input("Enter a positive whole number: ")
+num= float(num)
+num = abs(num)
+num= round(num)
+print(num)
+
+print(round(abs(float(input("Enter a positive whole number: ")))))
